b16142fb0336d1f5e541050d5a7c0cd6518019b5
Lucas Walter <wsacul@gmail.com>
2018-01-02 20:43:21 -0800

    Now have basic text display functionality, next font size control

--- rqt_topic_display/src/rqt_topic_display/topic_display.py
+++ rqt_topic_display/src/rqt_topic_display/topic_display.py
@@ -3,10 +3,15 @@ import rospkg
 import rospy
 
 from qt_gui.plugin import Plugin
+from python_qt_binding import QtCore
 from python_qt_binding import loadUi
-from python_qt_binding.QtWidgets import QWidget
+from python_qt_binding.QtCore import QTimer, Signal
+from python_qt_binding.QtWidgets import QLabel, QWidget
+from std_msgs.msg import String
+
 
 class TopicDisplay(Plugin):
+    do_update_label = QtCore.pyqtSignal(String)
 
     def __init__(self, context):
         super(TopicDisplay, self).__init__(context)
@@ -45,19 +50,37 @@ class TopicDisplay(Plugin):
         # Add widget to the user interface
         context.add_widget(self._widget)
 
+        # if self.topic_name
+        self.topic_name = "string"
+        self.update_topic()
+        self.label = self._widget.findChild(QLabel, 'label')
+        self.do_update_label.connect(self.update_label)
+
+
+    def handle_callback(self, msg):
+        self.do_update_label.emit(msg)
+
+    def update_label(self, msg):
+        self.label.setText(msg.data)
+
     def shutdown_plugin(self):
         # TODO unregister all publishers here
         pass
 
     def save_settings(self, plugin_settings, instance_settings):
-        # TODO save intrinsic configuration, usually using:
-        # instance_settings.set_value(k, v)
-        pass
+        instance_settings.set_value('topic', self.topic_name)
+
+    def update_topic(self):
+        rospy.loginfo("updating topic: " + self.topic_name)
+        self.sub = rospy.Subscriber(self.topic_name, String, self.handle_callback,
+                                    queue_size=1)
+
 
     def restore_settings(self, plugin_settings, instance_settings):
-        # TODO restore intrinsic configuration, usually using:
-        # v = instance_settings.value(k)
-        pass
+        if instance_settings.contains('topic'):
+            self.topic_name = instance_settings.value('topic')
+        rospy.loginfo('topic name: ' + self.topic_name)
+        self.update_topic()
 
     #def trigger_configuration(self):
         # Comment in to signal that the plugin has a way to configure
